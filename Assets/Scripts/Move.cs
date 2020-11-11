322c880e52e6df5f01ae3ad1f42eefb05a232cc8
Bennet Meier <bennet.meierqgmx.net>
2020-11-11 18:44:42 +0100

    Quasi alles

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float Speed = 0;
+
+    private void FixedUpdate()
+    {
+        Vector3 move = Vector3.zero;
+
+        // ---------- option 1 (bitte nicht so)
+
+        /*if (Input.GetKey(KeyCode.UpArrow))
+        {
+            move += Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            move += Vector3.down;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            move += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            move += Vector3.right;
+        }*/
+
+        // ---------- option 2 (so is besser)
+
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+
+        transform.position += move * Speed * .01f;
+    }
+}
